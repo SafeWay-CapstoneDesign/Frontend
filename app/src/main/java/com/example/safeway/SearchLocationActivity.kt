599542b33cc44196fd50ebcb79c2b0cd23a3e488
baebaej <korea024504@gmail.com>
2025-02-12 04:57:10 +0900

    feat: 위치 및 길안내 ui 구현 및 도착지 검색 ui 구현

---
+++ app/src/main/java/com/example/safeway/SearchLocationActivity.kt
@@ -0,0 +1,33 @@
+package com.example.safeway
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SearchLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_search_location)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed() // 새 방식으로 뒤로가기 동작 호출
+        }
+
+
+
+
+    }
+
+}
